1c41391a85bb8fa6cf60f07c6bc37cacb4165a34
Rachel <rachelkhoong@gmail.com>
2018-11-21 17:25:08 +1100

    Forgot to keep committing

---
+++ js/portfolioitems.js
@@ -0,0 +1,83 @@
+let portfolioItems = [
+  {
+    projectName: "Graded Pokemon",
+    projectSlug: 'gradedpokemon',
+    projectTags: 'htmlcss, javascript, php, mysql, wordpress, woocommerce',
+    projectThumb: 'img/thumb-gradedpokemon.jpg',
+    projectImage: 'img/image-gradedpokemon.jpg',
+    projectURL: 'http://gradedpokemon.rachelkhoong.com',
+    projectDescription: ''
+  },
+  {
+    projectName: "Canberra Games Society",
+    projectSlug: 'canberragamessociety',
+    projectTags: 'htmlcss, javascript, php, mysql, wordpress',
+    projectThumb: 'img/thumb-cgs.jpg',
+    projectImage: 'img/image-cgs.jpg',
+    projectURL: 'http://cgs.asn.au',
+    projectDescription: ''
+  },
+  {
+    projectName: "Terpsichore Dance Company",
+    projectSlug: 'terpsichoredancecompany',
+    projectTags: 'htmlcss, javascript, php, mysql, wordpress',
+    projectThumb: 'img/thumb-tdc.jpg',
+    projectImage: 'img/image-tdc.jpg',
+    projectURL: 'http://www.dancewithtdc.com.au',
+    projectDescription: ''
+  },
+  {
+    projectName: "Ammybunny",
+    projectSlug: 'ammybunny',
+    projectTags: 'htmlcss, javascript',
+    projectThumb: 'img/thumb-ammybunny.jpg',
+    projectImage: 'img/image-ammybunny.jpg',
+    projectURL: 'http://ammybunny.com',
+    projectDescription: ''
+  },
+  {
+    projectName: "Johnson Music School",
+    projectSlug: 'johnsonmusicschool',
+    projectTags: 'htmlcss',
+    projectThumb: 'img/thumb-jms.jpg',
+    projectImage: 'img/image-jms.jpg',
+    projectURL: 'http://johnsonmusic.com.au',
+    projectDescription: ''
+  },
+  {
+    projectName: "Employee Directory",
+    projectSlug: 'employeedirectory',
+    projectTags: 'htmlcss, javascript',
+    projectThumb: 'img/thumb-employee.jpg',
+    projectImage: 'img/image-employee.jpg',
+    projectURL: '',
+    projectDescription: ''
+  },
+  {
+    projectName: "Web Dashboard",
+    projectSlug: 'webdashboard',
+    projectTags: 'htmlcss, javascript',
+    projectThumb: 'img/thumb-dashboard.jpg',
+    projectImage: 'img/image-dashboard.jpg',
+    projectURL: '',
+    projectDescription: ''
+  },
+  {
+    projectName: "Wheel of Success",
+    projectSlug: 'wheelofsuccess',
+    projectTags: 'htmlcss, javascript',
+    projectThumb: 'img/thumb-wheel.jpg',
+    projectImage: 'img/image-wheel.jpg',
+    projectURL: '',
+    projectDescription: ''
+  },
+  {
+    projectName: "Searchable Photo Gallery",
+    projectSlug: 'searchablephotogallery',
+    projectTags: 'htmlcss, javascript',
+    projectThumb: 'img/thumb-gallery.jpg',
+    projectImage: 'img/image-gallery.jpg',
+    projectURL: '',
+    projectDescription: ''
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
 /* Javascript File */
+let filtercards = document.querySelector('.filtercards');
+$('.filtercards').on('click', (e)=>{
+  if (e.target.tagName==="LI"){
+    let allFilters = document.querySelectorAll('.filtercards li');
+    for (let i=0; i<allFilters.length; i++){
+      allFilters[i].classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+    let textvalue = e.target.innerHTML.toLowerCase();
+    if (textvalue === 'html/css'){ textvalue = 'htmlcss';}
+    if (textvalue === 'all'){ textvalue = '';}
+    $(".work-card").filter(function() {
+      if ( $(this).data('tags').toLowerCase().indexOf(textvalue) > -1 ){
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  }
+});
+
+//cycle through all portfolio card
+let workpage = document.querySelector('.work-page');
+for (let i=0; i<portfolioItems.length; i++){
+  let workCardHTML = `
+  <div class="work-card" data-tags="${portfolioItems[i].projectTags}" >
+    <div class="work-card-overflow">
+      <a href="works/${portfolioItems[i].projectSlug}.html">
+        <img src="${portfolioItems[i].projectThumb}" />
+        <span>${portfolioItems[i].projectName}</span>
+      </a>
+    </div>
+  </div>
+  `;
+  workpage.insertAdjacentHTML('beforeend',workCardHTML);
+};
+/*
+<div class="work-card" data-tags="tagshere" >
+  <div class="work-card-overflow">
+    <a href="works/johnsonmusicschool.html">
+      <img src="img/mark.jpg" />
+      <span>Johnson Music School</span>
+    </a>
+  </div>
+*/
