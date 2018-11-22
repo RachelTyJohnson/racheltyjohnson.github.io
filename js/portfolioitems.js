51e240efe1aee2573568c05275ef512812d3bdb3
Rachel <rachelkhoong@gmail.com>
2018-11-22 23:21:27 +1100

    Add descriptions

--- js/portfolioitems.js
+++ js/portfolioitems.js
@@ -6,7 +6,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-gradedpokemon.jpg',
     projectImage: 'img/image-gradedpokemon.jpg',
     projectURL: 'http://gradedpokemon.rachelkhoong.com',
-    projectDescription: 'Description for Graded Pokemon'
+    projectDescription: `Who hasn't had at least one Pokemon card when they were younger? I definitely had a bunch, which was a mix of legit cards and dodgy knockoffs. Who would've thunk that theres a whole secondary market for single Pokemon cards?</p><p>Graded Pokemon is a brand identity and website created for a client, one of US's leading sellers in graded single Pokemon cards. He was looking to move out of eBay and form an identity and website of his own. The branding was formed in cconjunction with the clients vision. The website uses WordPress for the blog section and WooCommerce for its online store.</p><p>NOTE: This is the version of the website hosted on my private server. The blog section is unreachable. The 5 products that exist are Mewtwo EX, Mewtwo EX, Mewtwo EX, Pikachu EX, and Kommo-o EX.`
   },
   {
     projectName: "Canberra Games Society",
@@ -15,7 +15,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-cgs.jpg',
     projectImage: 'img/image-cgs.jpg',
     projectURL: 'http://cgs.asn.au',
-    projectDescription: 'Description for Canberra Games Society'
+    projectDescription: `Every year, Australian gamers come together in what could be considered the Mecca of tabletop gaming. Cancon is run every January over the Australia Day weekend, and is the perfect snapshot of Australia gamers.</p><p>Canberra Games Society is the turning wheel behind the CanCon phenomena. They were in need of a website redesign and redevelopment, and that's where I came in. Working together with the existing logo and vision of the director, we came up with the new branding and website design. CGS's existing website was built on WordPress previously, and so the brief was to create something new on top of the backend framework that was already there. The redevelopment also introduced Advanced Custom Fields to increase ease for the person updating the website.`
   },
   {
     projectName: "Terpsichore Dance Company",
@@ -24,7 +24,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-tdc.jpg',
     projectImage: 'img/image-tdc.jpg',
     projectURL: 'http://www.dancewithtdc.com.au',
-    projectDescription: 'Description for Terpsichore Dance Company'
+    projectDescription: `As a little girl, I remember the first ballet class I ever took. I was about 5, I had a little pink tutu on, and I had the best time of my life running from corner to corner in my little ballet slippers, pretending to be a butterfly. I didn't take another dance class again until I was 23, at a fantastic local dance school that had an adults program: Terpsichore Dance Company.</p><p>This project is TDC's website for new and interested students, with links to their own student portal. It utilises Wordpress' Advanced Custom Fields and Custom Post Types to generate different pages of dance styles. It also sports a blog section that is powered by Wordpress itself. As my client maintains the website themselves, every piece of content is easily changeable in the WP admin area to make maintanence simple for her.`
   },
   {
     projectName: "Ammybunny",
@@ -33,7 +33,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-ammybunny.jpg',
     projectImage: 'img/image-ammybunny.jpg',
     projectURL: 'http://ammybunny.com',
-    projectDescription: 'Description for Ammy Bunny'
+    projectDescription: `Late 2017, I decided that a couple of baby bunnies would be the perfect birthday gift for my bunny-obsessed colleague. Little did I know that the adorable family additions would lead to a blooming side business selling adorable bunny merchandise.</p><p>AmmyBunny is a startup that sells mostly enamel pins featuring custom and adorable bunny art. This project is their landing website that leads to their various social profiles across the interwebs. The main feature of this website is the spinnable logo, which was a vision that my client was very adamant on. It was achieved using the propeller.js JavaScript plugin.`
   },
   {
     projectName: "Johnson Music School",
@@ -42,7 +42,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-jms.jpg',
     projectImage: 'img/image-jms.jpg',
     projectURL: 'http://johnsonmusic.com.au',
-    projectDescription: `Description for Johnson Music School`
+    projectDescription: `When I was a kid, one of the things my parents made sure I had a hand at was playing the piano. 13 years later I had achieved the highest level of piano possible, and it all started with a small independent music school. Not this one though, that'd be some amazing cyclical life event.</p><p>Johnson Music School is a small local music school that offer simple lessons for students of all ages and skill levels. This website is pure HTML and CSS with no back end or enhanced interactivity. The client had no need to change anything and so we agreed on a static setup.</p><p>I am currently working on expanding the website to have a Student Portal, and I plan to build this out using PHP and MySQL. The student portal is planned to house sheet music, homework, invoices and other documents. It will allow parents/students to log in to access what they need, and teachers to log in to upload files.`
   },
   {
     projectName: "Employee Directory",
@@ -51,7 +51,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-employee.jpg',
     projectImage: 'img/image-employee.jpg',
     projectURL: 'https://github.com/RachelTyJohnson/treehouse-fewd-project8',
-    projectDescription: 'Description for Employee Directory'
+    projectDescription: `This project was created as part of the Treehouse Front End Web Developer TechDegree. Here I worked with APIs for the first time. Using FetchAPI, I pulled 15 people from randomuser.me and displayed them in an "Employee Directory" layout. I utilized JavaScript objects quite a bit with this project and experienced populating a page using object properties. I found the display of a modal with dynamic content a thrill to create!`
   },
   {
     projectName: "Web Dashboard",
@@ -60,7 +60,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-dashboard.jpg',
     projectImage: 'img/image-dashboard.jpg',
     projectURL: 'https://github.com/RachelTyJohnson/treehouse-fewd-project7',
-    projectDescription: 'Description for Web Dashboard'
+    projectDescription: `This project was created as part of the Treehouse Front End Web Developer TechDegree. This was my first project using CSS Grids, I didn't think such a thing existed natively! I really enjoyed all the JavaScript interactions I included in this project, such as the notification bell and messaging section. This was also my first experience with using local storage, although I would much rather use something like MySQL to store user settings.`
   },
   {
     projectName: "Wheel of Success",
@@ -69,7 +69,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-wheel.jpg',
     projectImage: 'img/image-wheel.jpg',
     projectURL: 'https://github.com/RachelTyJohnson/treehouse-fewd-project6',
-    projectDescription: 'Description for Wheel of Success'
+    projectDescription: 'This project was created as part of the Treehouse Front End Web Developer TechDegree. This was such a fun project! It was the first time I saw JavaScript as a programming language where the flow could jump from function to function, just like any other program. The instructions provided to us were clear and really helped build the project from scratch. It helped me understand the process of building a JavaScript web app from scratch.'
   },
   {
     projectName: "Searchable Photo Gallery",
@@ -78,7 +78,7 @@ let portfolioItems = [
     projectThumb: 'img/thumb-gallery.jpg',
     projectImage: 'img/image-gallery.jpg',
     projectURL: 'https://github.com/RachelTyJohnson/treehouse-fewd-project5',
-    projectDescription: 'Description for Searchable Photo Gallery'
+    projectDescription: `This project was created as part of the Treehouse Front End Web Developer TechDegree. We were provided with images, thumbnails and captions to create a simple image gallery with lightbox. I had used a jQuery plugin for the lightbox, which was great practice in researching and implementing a third party plugin. I had a go at writing my own JavaScript code for the search function, and I surprised myself!`
   }
 ];
 
